49ff985e233ee03d73fa4a8b1780e5a0c7c112a9
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-20 16:30:52 -0300

    Add last sale response

---
+++ src/main/java/cl/transbank/pos/responses/integradoResponses/LastSaleResponse.java
@@ -0,0 +1,8 @@
+package cl.transbank.pos.responses.integradoResponses;
+
+public class LastSaleResponse extends SaleResponse {
+
+    public LastSaleResponse(String response) {
+        super(response);
+    }
+}
