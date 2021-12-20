4256d23f450cc4fa1390c9192e64871282fcdf43
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-20 00:55:12 -0300

    Remove unused exceptions

--- src/main/java/cl/transbank/pos/exceptions/TransbankPortNotConfiguredException.java
+++
@@ -1,7 +0,0 @@
-package cl.transbank.pos.exceptions;
-
-public class TransbankPortNotConfiguredException extends TransbankException {
-    public TransbankPortNotConfiguredException(String s) {
-        super(s);
-    }
-}
--- src/main/java/cl/transbank/pos/exceptions/TransbankUnexpectedError.java
+++
@@ -1,7 +0,0 @@
-package cl.transbank.pos.exceptions;
-
-public class TransbankUnexpectedError extends Error {
-    public TransbankUnexpectedError(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
