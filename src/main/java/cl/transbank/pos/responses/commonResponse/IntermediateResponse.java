b74bfe3b72c7e01e08387ad38765d2b81a0e07f9
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-19 23:53:49 -0300

    Add intermediate response

---
+++ src/main/java/cl/transbank/pos/responses/commonResponse/IntermediateResponse.java
@@ -0,0 +1,21 @@
+package cl.transbank.pos.responses.commonResponse;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter
+public class IntermediateResponse {
+    @Getter(AccessLevel.NONE)
+    private final BasicResponse basicResponse;
+
+    private final int functionCode;
+    private final int responseCode;
+    private final String responseMessage;
+
+    public IntermediateResponse(String response) {
+        basicResponse = new BasicResponse(response);
+        functionCode = basicResponse.getFunctionCode();
+        responseCode = basicResponse.getResponseCode();
+        responseMessage = basicResponse.getResponseMessage();
+    }
+}
