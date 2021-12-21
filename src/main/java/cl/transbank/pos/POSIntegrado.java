69c95e17050322e6cc9b2e4ae2e5dc50255d705d
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-21 16:35:52 -0300

    Implement POSIntegrado

--- src/main/java/cl/transbank/pos/POSIntegrado.java
+++ src/main/java/cl/transbank/pos/POSIntegrado.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
+@SuppressWarnings({"unused", "UnusedReturnValue"})
 public class POSIntegrado extends Serial {
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
     public boolean poll() throws TransbankSaleException {
         if(cantWrite()) {
             String exceptionMessage = String.format("Unable to Poll, can't write to port %s", port.getSystemPortName());
@@ -38,7 +38,6 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
     public boolean setNormalMode() throws TransbankSaleException {
         if(cantWrite()) {
             String exceptionMessage = String.format("Unable to set Normal Mode, can't write to port %s", port.getSystemPortName());
@@ -61,7 +60,6 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
     public LoadKeysResponse loadKeys() throws TransbankLoadKeysException {
         try {
             write("0800");
@@ -73,10 +71,10 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
     public SaleResponse sale(int amount, String ticket, boolean sendStatus) throws TransbankSaleException {
         if (amount < 50) throw new TransbankSaleException("Amount must be greater than 50.");
         if (amount > 999999999) throw new TransbankSaleException("Amount must be less than 999999999.");
+        if (ticket.trim().length() > 6) throw new TransbankSaleException("Ticket must be up to 6 in length");
 
         String command = String.format("0200|%s|%s|||%s|", amount, ticket, sendStatus ? 1 : 0);
 
@@ -90,7 +88,23 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
+    public MultiCodeSaleResponse multiCodeSale(int amount, String ticket, long commerceCode, boolean sendStatus) throws TransbankMultiCodeSaleException {
+        if (amount < 50) throw new TransbankMultiCodeSaleException("Amount must be greater than 50.");
+        if (amount > 999999999) throw new TransbankMultiCodeSaleException("Amount must be less than 999999999.");
+        if (ticket.trim().length() > 6) throw new TransbankMultiCodeSaleException("Ticket must be up to 6 in length");
+
+        String command = String.format("0200|%s|%s|||%s|%s|", amount, ticket, sendStatus ? 1 : 0, commerceCode);
+
+        try {
+            write(command, sendStatus);
+            MultiCodeSaleResponse response = new MultiCodeSaleResponse(currentResponse);
+            log.debug(response.toString());
+            return response;
+        } catch (TransbankException e) {
+            throw new TransbankMultiCodeSaleException("Unable to execute sale on pos", e);
+        }
+    }
+
     public LastSaleResponse lastSale() throws TransbankLastSaleException {
         try {
             write("0250|");
@@ -102,7 +116,17 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
+    public MultiCodeLastSaleResponse multiCodeLastSale() throws TransbankMultiCodeLastSaleException {
+        try {
+            write("0280|");
+            MultiCodeLastSaleResponse response = new MultiCodeLastSaleResponse(currentResponse);
+            log.debug(response.toString());
+            return response;
+        } catch (TransbankException e) {
+            throw new TransbankMultiCodeLastSaleException("Unable to recover last sale from pos", e);
+        }
+    }
+
     public RefundResponse Refund(int operationId) throws TransbankRefundException {
         String command = String.format("1200|%s|", operationId);
 
@@ -116,7 +140,6 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
     public TotalsResponse totals() throws TransbankTotalsException {
         try {
             write("0700||");
@@ -128,7 +151,6 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
     public List<DetailResponse> details(boolean printOnPOS) throws TransbankDetailException {
         String command = String.format("0260|%s|", printOnPOS ? 0 : 1);
         List<DetailResponse> details = new ArrayList<>();
@@ -146,7 +168,23 @@ public class POSIntegrado extends Serial {
         }
     }
 
-    @SuppressWarnings({"unused", "UnusedReturnValue"})
+    public List<MultiCodeDetailResponse> multiCodeDetails(boolean printOnPOS) throws TransbankMultiCodeDetailException {
+        String command = String.format("0260|%s|", printOnPOS ? 0 : 1);
+        List<MultiCodeDetailResponse> details = new ArrayList<>();
+
+        try {
+            write(command, false, true, printOnPOS);
+            for (String sale : saleDetailResponse) {
+                MultiCodeDetailResponse saleDetail = new MultiCodeDetailResponse(sale);
+                details.add(saleDetail);
+                log.debug(saleDetail.toString());
+            }
+            return details;
+        } catch (TransbankException e) {
+            throw new TransbankMultiCodeDetailException("Unable to request sale detail on pos", e);
+        }
+    }
+
     public CloseResponse close() throws TransbankCloseException {
         try {
             write("0800");
