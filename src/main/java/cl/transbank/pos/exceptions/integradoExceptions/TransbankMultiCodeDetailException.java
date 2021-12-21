49a52d4c0ef96427ba3944d286b00b320ff2b6cc
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-21 16:35:19 -0300

    Add multi code detail exception

---
+++ src/main/java/cl/transbank/pos/exceptions/integradoExceptions/TransbankMultiCodeDetailException.java
@@ -0,0 +1,5 @@
+package cl.transbank.pos.exceptions.integradoExceptions;
+
+public class TransbankMultiCodeDetailException extends TransbankDetailException {
+    public TransbankMultiCodeDetailException(String message, Throwable cause) {super(message, cause);}
+}
