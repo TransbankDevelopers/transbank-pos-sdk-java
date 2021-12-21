4c47d358815a148b6388d7e341edeec5deb096a3
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-21 15:55:20 -0300

    Add close response

---
+++ src/main/java/cl/transbank/pos/responses/integradoResponses/CloseResponse.java
@@ -0,0 +1,10 @@
+package cl.transbank.pos.responses.integradoResponses;
+
+import cl.transbank.pos.responses.commonResponses.LoadKeysResponse;
+
+public class CloseResponse extends LoadKeysResponse {
+
+    public CloseResponse(String response) {
+        super(response);
+    }
+}
