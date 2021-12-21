10518254d1f354081b9ebf95afb0b1e1a820ac3b
DarkFrost <m.astudillo.toledo@gmail.com>
2021-12-21 15:55:44 -0300

    Add multi code sale response

---
+++ src/main/java/cl/transbank/pos/responses/integradoResponses/MultiCodeSaleResponse.java
@@ -0,0 +1,42 @@
+package cl.transbank.pos.responses.integradoResponses;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static cl.transbank.pos.utils.ParameterParser.*;
+
+@Getter
+public class MultiCodeSaleResponse extends SaleResponse {
+    @Getter(AccessLevel.NONE)
+    private final Map<String, Integer> m_parameterMap = new HashMap<String, Integer>()
+    {
+        {
+            put("Filler", 19);
+            put("Change", 20);
+            put("CommerceProviderCode", 21);
+        }
+    };
+
+    private final String filler;
+    private final int change;
+    private final long commerceCode;
+
+    public MultiCodeSaleResponse(String response) {
+        super(response);
+        filler = parseStringParameter(m_response, m_parameterMap, "Filler");
+        change = parseIntParameter(m_response, m_parameterMap, "Change");
+        commerceCode = parseLongParameter(m_response, m_parameterMap, "CommerceCode");
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + "\n" +
+            "Filler: " + filler + "\n" +
+            "Change: " + change + "\n" +
+            "Commerce Provider Code: " + commerceCode;
+    }
+}
