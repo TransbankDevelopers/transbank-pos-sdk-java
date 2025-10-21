a3305ee3dee6bd0808d1c414bca7f3672e4b6828
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-10-21 14:30:09 -0300

    fix: printing field parse

--- src/main/java/cl/transbank/pos/utils/ParameterParser.java
+++ src/main/java/cl/transbank/pos/utils/ParameterParser.java
@@ -9,44 +9,50 @@ import java.util.Map;
 
 public class ParameterParser {
 
-    private ParameterParser() {}
+    private ParameterParser() {
+    }
 
     public static String parseStringParameter(String response, Map<String, Integer> parameterMap, String parameterKey) {
         try {
             return response.split("\\|")[parameterMap.get(parameterKey)].trim();
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            return "";
         }
-        catch (ArrayIndexOutOfBoundsException ex) { return ""; }
     }
 
     public static int parseIntParameter(String response, Map<String, Integer> parameterMap, String parameterKey) {
         try {
             return Integer.parseInt(response.split("\\|")[parameterMap.get(parameterKey)].trim());
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            return -1;
         }
-        catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) { return -1; }
     }
 
     public static long parseLongParameter(String response, Map<String, Integer> parameterMap, String parameterKey) {
         try {
             return Long.parseLong(response.split("\\|")[parameterMap.get(parameterKey)].trim());
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            return -1;
         }
-        catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) { return -1; }
     }
 
     public static Date parseAccountingDate(String response, Map<String, Integer> parameterMap) {
         try {
             String date = response.split("\\|")[parameterMap.get("AccountingDate")].trim();
             return new SimpleDateFormat("ddMMyyyy").parse(date);
+        } catch (ArrayIndexOutOfBoundsException | ParseException ex) {
+            return null;
         }
-        catch (ArrayIndexOutOfBoundsException | ParseException ex) { return null; }
     }
 
     public static Date parseRealDate(String response, Map<String, Integer> parameterMap) {
         try {
             String date = response.split("\\|")[parameterMap.get("RealDate")].trim();
             String hour = response.split("\\|")[parameterMap.get("RealTime")].trim();
-            return new SimpleDateFormat("ddMMyyyyHHmmss").parse(date+hour);
+            return new SimpleDateFormat("ddMMyyyyHHmmss").parse(date + hour);
+        } catch (ArrayIndexOutOfBoundsException | ParseException ex) {
+            return null;
         }
-        catch (ArrayIndexOutOfBoundsException | ParseException ex) { return null; }
     }
 
     public static List<String> parsePrintingField(String response, Map<String, Integer> parameterMap) {
@@ -59,19 +65,20 @@ public class ParameterParser {
                 return printingField;
             }
 
-            String fullPrintingField = arrayResponse[parameterMap.get("PrintingField")].trim();
+            String fullPrintingField = arrayResponse[parameterMap.get("PrintingField")];
 
-            if (fullPrintingField.length() % 40 != 0)
-            {
+            if (fullPrintingField.length() % 40 != 0) {
                 printingField.add(fullPrintingField);
                 return printingField;
             }
 
             for (int i = 0; i < fullPrintingField.length(); i += 40)
-                printingField.add(fullPrintingField.substring(i, 40));
+                printingField.add(fullPrintingField.substring(i, i + 40));
 
             return printingField;
 
-        } catch (IndexOutOfBoundsException e) { return printingField; }
+        } catch (IndexOutOfBoundsException e) {
+            return printingField;
+        }
     }
 }
