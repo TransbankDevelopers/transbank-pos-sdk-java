3f0990d878c612142ef648348c1da79459f817f9
Miguel Villalobos <miguel.villalobos@continuumhq.com>
2026-02-27 10:53:30 -0300

    docs: add javadoc for common exceptions

--- src/main/java/cl/transbank/pos/exceptions/common/TransbankLoadKeysException.java
+++ src/main/java/cl/transbank/pos/exceptions/common/TransbankLoadKeysException.java
@@ -1,5 +1,8 @@
 package cl.transbank.pos.exceptions.common;
 
+/**
+ * Exception thrown when the keys retrieval fails.
+ */
 public class TransbankLoadKeysException extends TransbankException{
 
     public TransbankLoadKeysException(String message, Throwable cause) {super(message, cause);}
